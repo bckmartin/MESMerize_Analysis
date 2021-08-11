1c07d4d688382e5f6caaad024319356ee29c4e9a
bckmartin <bckmartin2500@gmail.com>
2021-08-11 11:26:49 +0200

    1P motion correction notebook addded

--- caiman/1P_Motion_Corr.py
+++ caiman/1P_Motion_Corr.py
@@ -2,6 +2,7 @@
 # 1 - photon widefield imaging data
 # Example file is provided from the miniscope project page
 # www.miniscope.org
+import sys
 
 import numpy as np
 import caiman as cm
@@ -15,76 +16,86 @@ import matplotlib.pyplot as plt
 import cv2
 
 if __name__ == "__main__":
+    original_stdout = sys.stdout
+    with open('C:/Users/Martin/Desktop/Motion_correction_for/Motion_correction_offsets.txt', 'w') as f:
+        sys.stdout = f
+        my_lst = ['E:/Data/Experiment/2021/08.03/data/20210803-111355-Fluorescent-recording.tif',
+                  'E:/Data/Experiment/2021/08.03/data/20210803-111641-Fluorescent-recording.tif',
+                  'E:/Data/Experiment/2021/08.03/data/20210803-111820-Fluorescent-recording.tif',
+                  'E:/Data/Experiment/2021/08.03/data/20210803-123810-Fluorescent-recording.tif',
+                  'E:/Data/Experiment/2021/08.03/data/20210803-124444-Fluorescent-recording.tif',
+                  'E:/Data/Experiment/2021/08.03/data/20210803-124546-Fluorescent-recording.tif']
+        name_lst = ['C:/Users/Martin/Desktop/Motion_correction_for/3recording.png',
+                    'C:/Users/Martin/Desktop/Motion_correction_for/4recording.png',
+                    'C:/Users/Martin/Desktop/Motion_correction_for/5recording.png',
+                    'C:/Users/Martin/Desktop/Motion_correction_for/6recording.png',
+                    'C:/Users/Martin/Desktop/Motion_correction_for/7recording.png',
+                    'C:/Users/Martin/Desktop/Motion_correction_for/8recording.png']
+        for x in range(len(my_lst)):
+            path = my_lst[x]
+            print(x+3,". recording\n")
+            #download data and convert to single precision
+            fnames = [download_demo(path)]
+            #set up clusters for parallel processing
+            if 'dview' in locals():
+                cm.stop_server(dview=dview)
+            c, dview, n_processes = cm.cluster.setup_cluster(
+                backend='local', n_processes=None, single_thread=False)
 
-    #path = 'C:/Users/Martin/Downloads/20210729-141014-Fluorescent-recording.tif'
+            # dataset dependent parameters
+            frate = 35                       # movie frame rate
+            decay_time = 0.4                 # length of a typical transient in seconds
 
-    #download data and convert to single precision
-    #fnames = [download_demo(path)]
-    fnames = [download_demo('msCam13.avi')]
-    #set up clusters for parallel processing
-    if 'dview' in locals():
-        cm.stop_server(dview=dview)
-    c, dview, n_processes = cm.cluster.setup_cluster(
-        backend='local', n_processes=None, single_thread=False)
+            # motion correction parameters
+            motion_correct = True    # flag for performing motion correction
+            pw_rigid = False         # flag for performing piecewise-rigid motion correction (otherwise just rigid)
+            gSig_filt = (3, 3)       # size of high pass spatial filtering, used in 1p data
+            max_shifts = (5, 5)      # maximum allowed rigid shift
+            strides = (48, 48)       # start a new patch for pw-rigid motion correction every x pixels
+            overlaps = (24, 24)      # overlap between pathes (size of patch strides+overlaps)
+            max_deviation_rigid = 3  # maximum deviation allowed for patch with respect to rigid shifts
+            border_nan = 'copy'      # replicate values along the boundaries
 
-    # dataset dependent parameters
-    frate = 30                       # movie frame rate
-    decay_time = 0.4                 # length of a typical transient in seconds
+            mc_dict = {
+                'fnames': fnames,
+                'fr': frate,
+                'decay_time': decay_time,
+                'pw_rigid': pw_rigid,
+                'max_shifts': max_shifts,
+                'gSig_filt': gSig_filt,
+                'strides': strides,
+                'overlaps': overlaps,
+                'max_deviation_rigid': max_deviation_rigid,
+                'border_nan': border_nan
+            }
 
-    # motion correction parameters
-    motion_correct = True    # flag for performing motion correction
-    pw_rigid = False         # flag for performing piecewise-rigid motion correction (otherwise just rigid)
-    gSig_filt = (3, 3)       # size of high pass spatial filtering, used in 1p data
-    max_shifts = (5, 5)      # maximum allowed rigid shift
-    strides = (48, 48)       # start a new patch for pw-rigid motion correction every x pixels
-    overlaps = (24, 24)      # overlap between pathes (size of patch strides+overlaps)
-    max_deviation_rigid = 3  # maximum deviation allowed for patch with respect to rigid shifts
-    border_nan = 'copy'      # replicate values along the boundaries
+            opts = params.CNMFParams(params_dict=mc_dict)
 
-    mc_dict = {
-        'fnames': fnames,
-        'fr': frate,
-        'decay_time': decay_time,
-        'pw_rigid': pw_rigid,
-        'max_shifts': max_shifts,
-        'gSig_filt': gSig_filt,
-        'strides': strides,
-        'overlaps': overlaps,
-        'max_deviation_rigid': max_deviation_rigid,
-        'border_nan': border_nan
-    }
+            if motion_correct:
+                # do motion correction rigid
+                mc = MotionCorrect(fnames, dview=dview, **opts.get_group('motion'))
+                mc.motion_correct()
+                fname_mc = mc.fname_tot_els if pw_rigid else mc.fname_tot_rig
 
-    opts = params.CNMFParams(params_dict=mc_dict)
+                x_axis_elements = [x_shift[0] for x_shift in mc.shifts_rig]
+                y_axis_elements = [y_shift[1] for y_shift in mc.shifts_rig]
 
-    if motion_correct:
-        # do motion correction rigid
-        mc = MotionCorrect(fnames, dview=dview, **opts.get_group('motion'))
-        mc.motion_correct(save_movie=True)
-        fname_mc = mc.fname_tot_els if pw_rigid else mc.fname_tot_rig
-        if pw_rigid:
-            bord_px = np.ceil(np.maximum(np.max(np.abs(mc.x_shifts_els)),
-                                         np.max(np.abs(mc.y_shifts_els)))).astype(np.int)
-        else:
-            bord_px = np.ceil(np.max(np.abs(mc.shifts_rig))).astype(np.int)
-            plt.subplot(1, 2, 1); plt.imshow(mc.total_template_rig)  # % plot template
-            plt.subplot(1, 2, 2); plt.plot(mc.shifts_rig)  # % plot rigid shifts
-            plt.legend(['x shifts', 'y shifts'])
-            plt.xlabel('frames')
-            plt.ylabel('pixels')
-            plt.show(block=True)
-            print("rigid shift list index 1 type: ", type(mc.shifts_rig[0][0]))
-            print("from the 1st item of the rigid shift list, the max of the 1st part of the tuple", np.max(mc.shifts_rig[0][0]))
-            print("from the 1st item of the rigid shift list, the max of the 2nd part of the tuple", np.max(mc.shifts_rig[0][1]))
+                print("Max x oriented shift(px): ", np.max(x_axis_elements))
+                print("Max y oriented shift(px)? ", np.max(y_axis_elements))
 
-        bord_px = 0 if border_nan is 'copy' else bord_px
-        fname_new = cm.save_memmap(fname_mc, base_name='memmap_', order='C',
-                                   border_to_0=bord_px)
-    else:  # if no motion correction just memory map the file
-        fname_new = cm.save_memmap(fnames, base_name='memmap_',
-                                   order='C', border_to_0=0, dview=dview)
+                print("Min x oriented shift(px): ", np.min(x_axis_elements))
+                print("Min y oriented shift(px)? ", np.min(y_axis_elements), "\n\n")
 
-    inspect_results = True
-    if inspect_results:
-        cm.concatenate((cm.load(fnames), cm.load(mc.mmap_file)), axis=0).play()
 
-    #cm.load(mc.mmap_file)
+
+                fig = plt.figure()
+                plt.plot(mc.shifts_rig)  # % plot rigid shifts
+                plt.legend(['x shifts', 'y shifts'])
+                plt.xlabel('frames')
+                plt.ylabel('pixels')
+                #plt.show(block=True)
+                plt.savefig(name_lst[x])
+        sys.stdout = original_stdout
+
+
+
--- caiman/main.py
+++ caiman/main.py
@@ -9,28 +9,7 @@ if __name__ == "__main__":
     #print(a.__dict__)
     #print(a.headers)
 
-    Arr = np.array(
-        [1, 3, 2, 6,
-         7, 4, 2, 5,
-         7, 2, 4, 6,
-         2, 5, 7, 2,
-         5, 6, 2, 3,
-         5, 67, 2, 4,
-         5, 6, 2, 3,
-         5, 6, 1, 6,
-         8, 2, 3, 7])
-    #n = 4
-    start_frames = np.array([0, 4, 8, 12, 16, 20, 24, 28, 32])
-    x = averaging_by_frames(Arr, start_frames)
-    print(x)
-    """
-    print(Avg_arr[0])
-    print(Avg_arr[1])
-    print(Avg_arr[2])
-    print(Avg_arr[3])
-    plt.plot(Avg_arr)
-    plt.show(block=True)
-    """
+    print("Hello")
 
 
 
